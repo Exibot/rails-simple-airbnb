949c3bd4efa6869fed84aa213bb7b013ef44d5aa
Enrique Altamirano <enrique.altamiros@gmail.com>
2022-11-26 12:29:01 -0300

    actualizando simple airbnb

--- Gemfile
+++ Gemfile
@@ -27,6 +27,12 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Gemas personales
+
+gem "simple_form", github: "heartcombo/simple_form"
+
+gem 'faker', '~> 1.6', '>= 1.6.6'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+# 20.times do
+#   Flat.create!(
+#     name: Faker::Company.name,
+#     address: Faker::Address.street_address,
+#     description: Faker::Lorem.paragraph,
+#     price_per_night: Faker::Commerce.price(range: 20.0..100.0),
+#     number_of_guests: Faker::Number.number(from: 1, to: 4)
+#   )
+# end
+
+Flat.create!(
+  name: Faker::Company.name,
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
